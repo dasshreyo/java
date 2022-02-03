5578c1be5907ed526b6998b94de539af90d81eb6
dasshreyo <88198411+dasshreyo@users.noreply.github.com>
2022-02-03 16:07:55 +0530

    Add files via upload

---
+++ NumberCalculation.java
@@ -0,0 +1,19 @@
+
+package Shreyosri;
+import java.util.Scanner;
+public class NumberCalculation {
+	
+	 
+	 
+	 public static void main(String args[]){
+		 int a,b;
+	 Scanner sc=new Scanner(System.in);
+	 a=sc.nextInt();
+	 b=sc.nextInt();
+	 System.out.println((a+b));
+	 System.out.println((a-b));
+	 System.out.println((a*b));
+	 System.out.println((a/b));
+	 }
+	 }
+
