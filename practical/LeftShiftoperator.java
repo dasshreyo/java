29d9ef1ab5667024b1df389d7927199a6d47f5e1
dasshreyo <88198411+dasshreyo@users.noreply.github.com>
2022-02-03 20:10:53 +0530

    Add files via upload

---
+++ practical/LeftShiftoperator.java
@@ -0,0 +1,15 @@
+package Shreyosri;
+
+public class LeftShiftoperator {
+	
+	public static void main(String args[]) 
+	{ 
+	byte x = 64; 
+	int i; 
+	byte y; 
+	i = x << 2;
+	 y = (byte) (x << 2) ;
+	System.out.print(i + " " + y); 
+	} }
+
+
