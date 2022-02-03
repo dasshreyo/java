a3c7d81d5adcd917f5b56fc40ac86565e38b217a
dasshreyo <88198411+dasshreyo@users.noreply.github.com>
2022-02-03 16:06:53 +0530

    Add files via upload

---
+++ assignment/ProductNumber.java
@@ -0,0 +1,13 @@
+package Shreyosri;
+
+public class ProductNumber {
+
+		 int a=4, b=5, c=6, p=1;
+		 public static void main(String[] args) {
+			 int a=4, b=5, c=6, p=1;
+		 p =a*b*c;
+		 System.out.println(p);
+		 }
+		 }
+
+
