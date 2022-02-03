c837afb4b1f971b187cfde2ee0e8369732bb358c
dasshreyo <88198411+dasshreyo@users.noreply.github.com>
2022-02-03 20:12:51 +0530

    Add files via upload

---
+++ practical/Box.java
@@ -0,0 +1,21 @@
+package Shreyosri;
+
+public class Box {
+	
+	 int width;
+	 int height; 
+	int length; 
+	} 
+	class mainclass 
+	{
+	 public static void main(String args[]) 
+	{
+	 Box obj = new Box(); 
+	obj.width = 10; 
+	obj.height = 2;
+	 obj.length = 10;
+	 int y = obj.width * obj.height * obj.length;
+	 System.out.print(y); 
+	} }
+
+
