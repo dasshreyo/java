b41c337a10230a229fa3513cf7330cb5fdd7c14a
dasshreyo <88198411+dasshreyo@users.noreply.github.com>
2022-02-03 16:09:32 +0530

    Add files via upload

---
+++ assignment/ArrayTest2.java
@@ -0,0 +1,12 @@
+package Shreyosri;
+
+public class ArrayTest2 {
+	public static void main(String[] args)
+	{
+   	 String str[] = { "Geeks", "for", "Geeks" };
+   	 for (int i = 0; i < str.length; i++)
+        System.out.print(str[i]);
+	}
+}
+
+
