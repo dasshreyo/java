85625b36973ebc63d7ebeafcc48e0bfeae721bad
dasshreyo <88198411+dasshreyo@users.noreply.github.com>
2022-02-03 14:58:07 +0530

    Add files via upload

---
+++ practical/MultidimensionalArray.java
@@ -0,0 +1,16 @@
+package Shreyosri;
+
+public class MultidimensionalArray {
+	public static void main(String args[]){
+	int[][] multidimensionalArray = { {1,2},{2,3}, {3,4} };
+    
+    for(int i = 0 ; i < 3 ; i++){
+       //row
+       for(int j = 0 ; j < 2; j++){
+          System.out.print(multidimensionalArray[i][j] + " ");
+       }
+       System.out.println();
+    }
+
+}
+}
