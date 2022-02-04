adf79c9adfc5deec7e8a107a32b85ab28889fe9e
dasshreyo <88198411+dasshreyo@users.noreply.github.com>
2022-02-04 14:29:38 +0530

    Add files via upload

---
+++ practical/VariableScope.java
@@ -0,0 +1,16 @@
+package Shreyosri;
+
+public class VariableScope {
+	
+
+	 public static void main(String args[])
+	 { 
+	int x;
+	x = 5; 
+	
+	int y = 6; 
+	System.out.println(x + " " + y); 
+	
+	System.out.println(x + " " + y);
+	 }
+	 }
