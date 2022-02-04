be0c852d0c64858db9bad697a5e31dde09b8cd11
dasshreyo <88198411+dasshreyo@users.noreply.github.com>
2022-02-04 16:14:50 +0530

    Add files via upload

---
+++ practical/DecOrder.java
@@ -0,0 +1,19 @@
+package Shreyosri;
+
+import java.util.Arrays;
+
+public class DecOrder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String [] array = {"Mango","Apple","Grapes","Papaya","Pineapple","Bannana","Orange"};   
+		   
+		Arrays.sort(array);  
+		System.out.println("Array elements in descending order: " +Arrays.toString(array));
+
+	}
+
+
+	}
+
+
