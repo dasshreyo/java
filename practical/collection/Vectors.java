0b6a61e37e7f906a5917e7ca79715ed9931520e0
dasshreyo <88198411+dasshreyo@users.noreply.github.com>
2022-02-08 15:39:17 +0530

    Add files via upload

---
+++ practical/collection/Vectors.java
@@ -0,0 +1,18 @@
+package Shreyosri;
+import java.util.*;
+public class Vectors{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Vector<String> v=new Vector<String>();  
+		v.add("Ayush");  
+		v.add("Amit");  
+		v.add("Ashish");  
+		v.add("Garima");  
+		Iterator<String> itr=v.iterator();  
+		while(itr.hasNext()){  
+		System.out.println(itr.next());  
+		}
+	}
+
+}
