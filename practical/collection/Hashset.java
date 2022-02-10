a144a8c91ee28a44a66985df1e0c3feed5432581
dasshreyo <88198411+dasshreyo@users.noreply.github.com>
2022-02-10 14:44:51 +0530

    Add files via upload

---
+++ practical/collection/Hashset.java
@@ -0,0 +1,20 @@
+package Shreyosri;
+import java.util.*;
+public class Hashset {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HashSet<String> set=new HashSet();  
+        set.add("One");    
+        set.add("Two");    
+        set.add("Three");   
+        set.add("Four");  
+        set.add("Five");  
+        Iterator<String> i=set.iterator();  
+        while(i.hasNext())  
+        {  
+        System.out.println(i.next());  
+        }
+	}
+
+}
