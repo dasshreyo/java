a126f07af30598817a1fe419feb7a0509c5870bf
dasshreyo <88198411+dasshreyo@users.noreply.github.com>
2022-02-16 14:57:34 +0530

    Add files via upload

---
+++ practical/sql/table create insert.sql
@@ -0,0 +1,16 @@
+create database rima;
+
+ use riya;
+ 
+  
+  
+  desc student;
+  select * from student;
+
+insert into student values(1,'shreyosri','joynagar');
+insert into student values(2,'sayani','garia');
+insert into student values(5,'akash','dakshin barasat');
+insert into student values(6,'abir','dakshin barasat');
+insert into student values(3,'rima','joynagar');
+insert into student values(4,'riya','baharu');
+ select * from student;
