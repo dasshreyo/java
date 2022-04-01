f0fda595f63187c86fdbf261f10db28d05e0c37d
dasshreyo <88198411+dasshreyo@users.noreply.github.com>
2022-04-01 18:23:49 +0530

    Add files via upload

---
+++ Lab activity/Manager.java
@@ -0,0 +1,16 @@
+
+public class Manager  extends Employee {
+	 public Manager(int id, String name, String address, long phone,double salary){
+		 super(id, name, address, phone);
+			
+		 this.employeeid=id;
+		   this.employeeName=name;
+		   this.employeeAddress=address;
+		   this.employeephone=phone;
+		}
+
+			
+		}
+
+	 
+
---
+++ Lab activity/Trainee.java
@@ -0,0 +1,11 @@
+
+public class Trainee extends Employee {
+public Trainee(long id,String name,String address,long phone,double salary) {
+	super(id,name,address,phone);
+	this.employeeAddress=address;
+	this.employeeName=name;
+	this.employeeid=id;
+	this.employeephone=phone;
+	
+}
+}
