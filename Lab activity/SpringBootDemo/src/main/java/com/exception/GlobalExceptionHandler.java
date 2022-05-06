c53fb850f2b885ea68d84bfab9b17dd672acd813
dasshreyo <88198411+dasshreyo@users.noreply.github.com>
2022-05-06 12:05:10 +0530

    Add files via upload

---
+++ Lab activity/SpringBootDemo/src/main/java/com/exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler
+{
+
+	@ExceptionHandler(BookNotFoundException.class)
+	public void springHandleNotFound(HttpServletResponse response) throws IOException
+	{
+		response.sendError(HttpStatus.NOT_FOUND.value());
+	}
+
+	
+}
